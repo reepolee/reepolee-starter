f44a9aa04257c15326e64f400bb0964566fa1b63
alesvaupotic <ales@vaupotic.eu>
2021-01-19 14:47:39 +0100

    nollup server all ips

--- .nolluprc.js
+++ .nolluprc.js
@@ -1,7 +1,8 @@
 module.exports = {
   hot: true,
-  contentBase: 'assets',
-  publicPath: 'build',
-  historyApiFallback: '__app.html',
-  port: 5000
-}
+  contentBase: "assets",
+  publicPath: "build",
+  historyApiFallback: "__app.html",
+  port: 5000,
+  host: "0.0.0.0",
+};
